aea7504b339ee41fdce6c99f6dfda12c490a01e7
Sarah Osama <hylrcs@gmail.com>
2022-01-10 18:38:51 +0200

    ..

--- src/interfaces/IUser.java
+++ src/interfaces/IUser.java
@@ -70,6 +70,14 @@ public class IUser {
         return customerService.editProfile(this,updatedUser);
 
     }
+    public List<IBook> getBooks(){
+        List<IBook> res =  bookService.getBooks();
+        for (IBook re : res) {
+            System.out.println(re.toString());
+        }
+        return res;
+    }
+
     public List<IBook> searchBook(String searchWords){
         List<IBook> res =  bookService.findBook(searchWords);
         for (IBook re : res) {
